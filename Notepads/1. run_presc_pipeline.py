a0f5ba46c1218e7d549f353f1ce32711f401f9d3
Vikram Solanki <134689573+solanki1750@users.noreply.github.com>
2023-06-02 17:45:43 +0530

    Create 1. run_presc_pipeline.py

---
+++ Notepads/1. run_presc_pipeline.py
@@ -0,0 +1,41 @@
+### Import all the necessary Modules
+
+def main():
+    ### Get All Variables
+
+    ### Get Spark Object
+        # Validate Spark Object
+        # Set up Logging Configuration Mechanism
+        # Set up Error Handling
+
+    ### Initiate run_presc_data_ingest Script
+        # Load the City File
+        # Load the Prescriber Fact File
+        # Validate
+        # Set up Logging Configuration Mechanism
+        # Set up Error Handling
+
+    ### Initiate run_presc_data_preprocessing Script
+        # Perform data Cleaning Operations
+        # Validate
+        # Set up Logging Configuration Mechanism
+        # Set up Error Handling
+
+    ### Initiate run_presc_data_transform Script
+        # Apply all the transfrmations Logics
+        # Validate
+        # Set up Logging Configuration Mechanism
+        # Set up Error Handling
+
+    ### Initiate run_data_extraction Script
+        # Validate
+        # Set up Logging Configuration Mechanism
+        # Set up Error Handling
+
+### End of Application Part 1
+
+
+
+
+if __name__ == "__main__" :
+    main()
