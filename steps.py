c992c86d3b3ccb12d662f1568a2b5030344be12c
Vikram Solanki <134689573+solanki1750@users.noreply.github.com>
2023-06-29 16:44:28 +0530

    Update steps.py

--- steps.py
+++ steps.py
@@ -1,6 +1,10 @@
+# Initiate PySpark on myMackbook air
 cd documents
-
 cd spark-3.4.1-bin-hadoop3
-
 bin/pyspark
+
+# Initiate Google CLI on myMacbook air
+./google-cloud-sdk/bin/gcloud init
+#  authenticate with login to google cloud account
+
 
